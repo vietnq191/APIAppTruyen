276b64875fd0c71d81b064079d92013aa526f2c1
Nguyen Viet <86089143+voyen057@users.noreply.github.com>
2022-12-30 11:55:58 +0700

    Add story viewed on categories list

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,16 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Categories::all();
+        $categories = Categories::all()->toArray();
+        $data_history = [
+            "id"=> -1,
+            "category_name" => "Truyện đã xem",
+            "description" => "",
+            "deleted_at" => null,
+            "created_at" => null,
+            "updated_at" => null
+        ];
+        array_unshift($categories, $data_history);
         return response($categories);
     }
 }
